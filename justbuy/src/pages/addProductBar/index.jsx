2ccf5de94903900e1b13f1365d907db7e8e167cd
CUKENGER <andreychuvashov2015@gmail.com>
2023-09-01 15:33:57 +0300

    blablabla

---
+++ justbuy/src/pages/addProductBar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import addProductBar from "../../components/addProduct/addProductBar"
+
+const addProduct = () => {
+    return (
+        <main className="addProduct">
+            <div className="container">
+                <addProductBar/>
+            </div>
+        </main>
+    )
+}
+
+export default addProduct;
--- justbuy/src/router/routes.js
+++ justbuy/src/router/routes.js
@@ -1,6 +1,6 @@
 import Index from "../pages/index";
 import Profile from "../pages/profile";
-
+import addProduct from "../pages/addProductBar";
 
 
 
@@ -9,4 +9,5 @@ export const publicRoutes = [
     {path: '/', component: Index, exact: true},
     {path: '/index', component: Index, exact: true},
     {path: '/profile', component: Profile, exact: true},
+    {path: '/addproduct', component: addProduct, exact: true}
 ]
